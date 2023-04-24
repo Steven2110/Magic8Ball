829d09d3901be48b4c5a7ffcb7e1ee97896dcbf9
Steven Wijaya <steven.wijaya2001@gmail.com>
2023-04-25 01:56:49 +0700

    Initial Commit

---
+++ Magic8Ball/Magic8BallApp.swift
@@ -0,0 +1,17 @@
+//
+//  Magic8BallApp.swift
+//  Magic8Ball
+//
+//  Created by Steven Wijaya on 25.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct Magic8BallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
